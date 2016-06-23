a3e642911b5a695068fd42d569f340e2b5def0f6
yasmin <yasmin_martos@hotmail.com>
2016-06-23 10:29:34 -0500

    Arquitectura en capas

---
+++ AppEnvioRecepcionOfertas/AppEnvioRecepcionOfertas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AppEnvioRecepcionOfertas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
