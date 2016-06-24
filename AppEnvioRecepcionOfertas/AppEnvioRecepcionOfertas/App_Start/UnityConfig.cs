814739140fcee94c768081af81683623aefa9781
Cristofer Bardales Vargas <jonyoc6@outlook.com>
2016-06-24 09:30:23 -0500

    .

--- AppEnvioRecepcionOfertas/AppEnvioRecepcionOfertas/App_Start/UnityConfig.cs
+++ AppEnvioRecepcionOfertas/AppEnvioRecepcionOfertas/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
+using AppEnvioRecepcionOfertas.Controllers;
 
 namespace AppEnvioRecepcionOfertas.App_Start
 {
@@ -37,6 +38,8 @@ namespace AppEnvioRecepcionOfertas.App_Start
 
             // TODO: Register your types here
             // container.RegisterType<IProductRepository, ProductRepository>();
+            //container.RegisterType<AccountController>(new InjectionConstructor());
+            //container.RegisterType<AccountController>(new InjectionConstructor()); container.RegisterType<RolesAdminController>(new InjectionConstructor()); container.RegisterType<ManageController>(new InjectionConstructor()); container.RegisterType<UsersAdminController>(new InjectionConstructor());
         }
     }
 }
